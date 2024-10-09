af47f020bd97e9ee4da5c345bbcf7ef0874cf55d
SuperiorDrey <132139152+Khaurie@users.noreply.github.com>
2024-10-10 00:01:52 +0800

    Add files via upload

---
+++ components/footer/index.tsx
@@ -0,0 +1,56 @@
+import Logo from '../../assets/icons/logo';
+
+const Footer = () => {
+  return (
+    <footer className="site-footer">
+      <div className="container">
+        <div className="site-footer__top">
+          <div className="site-footer__description">
+            <h6><Logo /> <span>E</span>-Shop</h6>
+            <p>Welcome to Sasa Pet Shop—your one-stop online store for all things pets! We offer a wide range of high-quality products, including food, toys, accessories, and grooming essentials for dogs, cats, birds, and more. Our mission is to provide everything your furry, feathered, or scaly friends need to live happy and healthy lives. Shop with us for affordable prices, fast delivery, and excellent customer service that truly cares for your pet’s well-being!</p>
+            <ul className="site-footer__social-networks">
+              <li><a href="#"><i className="icon-facebook"></i></a></li>
+              <li><a href="#"><i className="icon-twitter"></i></a></li>
+              <li><a href="#"><i className="icon-linkedin"></i></a></li>
+              <li><a href="#"><i className="icon-instagram"></i></a></li>
+              <li><a href="#"><i className="icon-youtube-play"></i></a></li>
+            </ul>
+          </div>
+
+          <div className="site-footer__links">
+            <ul>
+              <li>Shopping online</li>
+              <li><a href="#">Order Status</a></li>
+              <li><a href="#">Shipping and Delivery</a></li>
+              <li><a href="#">Returns</a></li>
+              <li><a href="#">Payment options</a></li>
+              <li><a href="#">Contact Us</a></li>
+            </ul>
+            <ul>
+              <li>Information</li>
+              <li><a href="#">Gift Cards</a></li>
+              <li><a href="#">Find a store</a></li>
+              <li><a href="#">Newsletter</a></li>
+              <li><a href="#">Bacome a member</a></li>
+              <li><a href="#">Site feedback</a></li>
+            </ul>
+            <ul>
+              <li>Contact</li>
+              <li><a href="#">jsperdido20@gmail.com</a></li>
+              <li><a href="#">Hotline: +63-968-545-8991</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      
+      <div className="site-footer__bottom">
+        <div className="container">
+          <p>DESIGN BY @Icreatechs_DevDrian - © 2024. ALL RIGHTS RESERVED.</p>
+        </div>
+      </div>
+    </footer>
+  )
+};
+
+
+export default Footer
---
+++ utils/data/products-types.ts
@@ -0,0 +1,24 @@
+export const productsTypes = [
+  {
+    id: '1',
+    name: 'Food',
+    count: '172',
+  },
+  {
+    id: '2',
+    name: 'Accessory',
+    count: '131',
+  },
+  {
+    id: '3',
+    name: 'Eqiupment',
+    count: '56',
+  },
+  {
+    id: '4',
+    name: 'Shampoo',
+    count: '8',
+  },
+];
+
+export default productsTypes;
